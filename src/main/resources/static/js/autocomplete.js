c16ed2b834654232317a26b8f6a9b21745b9dbaa
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-08 16:00:42 +0900

    パスを編集

--- src/main/resources/static/js/autocomplete.js
+++ src/main/resources/static/js/autocomplete.js
@@ -4,11 +4,11 @@ $(function(){
 
     $.ajax({
         /** オートコンプリート用のリストを取得するURL*/
-        url: "/" + pathname + "/getAutoComplete",
+        url: "/" + pathname + "/employee/getAutoComplete",
         dataType: "json",
         type: 'GET'
     }).then(function(searchResult){ /** 検索結果のリストは引数に自動で入る*/
-        $('.txtKeywd').autocomplete({ /** 実装したい入力欄のid/classを指定*/
+        $('#showName').autocomplete({ /** 実装したい入力欄のid/classを指定*/
             source : searchResult, /** 候補リスト(必須)*/
             autoFocus: true, /**リスト表示に際してリスト先頭にフォーカスするか*/
             delay: 500, /**キー入力からリスト表示までの遅延時間（ミリ秒）*/
